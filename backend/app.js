25eba21859dfe34a9975eb4ea80b894b5a411513
oreste-abizera <oresteabizera11@gmail.com>
2021-05-03 14:33:14 +0200

    handling connections

--- backend/app.js
+++ backend/app.js
@@ -10,6 +10,26 @@ app.use(index);
 
 const server = http.createServer(app);
 
-const io = socketIo(server); // < Interesting!
+const io = socketIo(server);
 
-const getApiAndEmit = "TODO";
+let interval;
+
+io.on("connection", (socket) => {
+  console.log("New client connected");
+  if (interval) {
+    clearInterval(interval);
+  }
+  interval = setInterval(() => getApiAndEmit(socket), 1000);
+  socket.on("disconnect", () => {
+    console.log("Client disconnected");
+    clearInterval(interval);
+  });
+});
+
+const getApiAndEmit = (socket) => {
+  const response = new Date();
+  // Emitting a new message. Will be consumed by the client
+  socket.emit("Transactions", response);
+};
+
+server.listen(port, () => console.log(`Listening on port ${port}`));
