b9b91eb7483c09fdf26315f4e42f9ff847794524
oreste-abizera <oresteabizera11@gmail.com>
2021-05-03 16:41:48 +0200

    Designing record transaction form

--- frontend/src/routes/create/index.tsx
+++ frontend/src/routes/create/index.tsx
@@ -6,6 +6,14 @@ const Create: FunctionalComponent = () => {
   return (
     <div class={style.createPage}>
       <h1>Record a transaction</h1>
+      <form>
+        <input type="text" placeholder="card id" required></input>
+        <input type="number" placeholder="initial balance" required></input>
+        <input type="number" placeholder="transaction fare" required></input>
+        <input type="number" placeholder="new balance" required></input>
+
+        <input type="submit">Record</input>
+      </form>
       <Link href="/">
         <h4>Return to Home</h4>
       </Link>
